a9534be66ec4024c49531718f527f5942b22b6b3
FahaF <farihatasnimchy@gmail.com>
2023-02-05 21:45:36 +0600

    initial commit

---
+++ src/components/EnterButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <router-link to="/topics">
+        <Button color="teal" size="massive" content="Let Start"></Button>
+    </router-link>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Button } from 'semantic-ui-vue';
+
+@Component({
+    components: {
+        Button,
+    },
+})
+export default class EnterButton extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
---
+++ src/components/TopicList.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul class="list">
+    <li class="list__item" v-for="topic in topics" :key="topic.slug">
+      <item :topic="topic"></item>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.list {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  max-width: 1400px;
+}
+
+.list__item {
+  margin: 10px;
+  padding: 0;
+  flex: 1 0 25%;
+  min-width: 300px;
+  min-height: 16vh;
+  max-width: 400px;
+  outline: 1px solid #fff;
+}
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TopicItem from './TopicItem.vue';
+
+@Component({
+  components: {
+    item: TopicItem,
+  },
+  props: {
+    topics: {
+      type: Array,
+      default: [],
+    },
+  },
+})
+export default class TopicList extends Vue {}
+</script>
+
+
